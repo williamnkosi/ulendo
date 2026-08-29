901a8119f7bc8c4bf3e21fde04c3f76fa4ae46c4
William Nkosi <william.dev.acc@gmail.com>
2026-08-29 13:44:26 -0400

    ULENDO-1/monorepo-setup: Created shared packages

---
+++ packages/ulendo_models/test/ulendo_models_test.dart
@@ -0,0 +1,16 @@
+import 'package:ulendo_models/ulendo_models.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
